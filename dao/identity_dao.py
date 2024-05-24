8b778cca5a7675af493106bbd05467eb4429d4d0
yash-pathak-1997 <yashhpathakk1997@gmail.com>
2024-05-24 22:51:20 +0530

    Code cleanup - Reducing variables in dao layer

--- dao/identity_dao.py
+++ dao/identity_dao.py
@@ -7,50 +7,47 @@ from api import db
 
 
 def identity_dao(identity_request: IdentityRequestModel):
-    # Extract email and phoneNumber from the IdentityRequestModel
-    email = identity_request.email
-    phoneNumber = identity_request.phoneNumber
+    if identity_request.email and identity_request.phoneNumber:
+        email_contacts = Contact.query.filter(Contact.email == identity_request.email).all()
+        phoneNumber_contacts = Contact.query.filter(Contact.phoneNumber == identity_request.phoneNumber).all()
 
-    # Find all objects
-    email_contacts = Contact.query.filter(Contact.email == email).all()
-    phoneNumber_contacts = Contact.query.filter(Contact.phoneNumber == phoneNumber).all()
+        # Case 1: Check if contact exist for incoming request
+        if not email_contacts or not phoneNumber_contacts:
+            if not email_contacts and not phoneNumber_contacts:
+                add_as_primary(identity_request)
+            else:
+                add_as_secondary(identity_request)
 
-    # Case 1: Check if contact exist for incoming request
-    if not email_contacts or not phoneNumber_contacts:
-        if not email_contacts and not phoneNumber_contacts:
-            add_as_primary(identity_request)
-        else:
-            add_as_secondary(identity_request)
+        # Case 2: Check if primary can turn secondary
+        """
+            Step 1- Find min id object for both email and phoneNumber
+            Step 2- Check if id is same. No action if same id
+            Step 3- If id is different, 
+                    change the larger id's linkPrecedence to secondary
+                    add smaller id in its linkedId
+        """
+        min_email_contact = Contact.query.filter(Contact.email == identity_request.email).order_by(Contact.id).first()
+        min_phone_contact = Contact.query.filter(Contact.phoneNumber == identity_request.phoneNumber).order_by(Contact.id).first()
 
-    # Case 2: Check if primary can turn secondary
-    """
-        Step 1- Find min id object for both email and phoneNumber
-        Step 2- Check if id is same. No action if same id
-        Step 3- If id is different, 
-                change the larger id's linkPrecedence to secondary
-                add smaller id in its linkedId
-    """
-    min_email_contact = Contact.query.filter(Contact.email == email).order_by(Contact.id).first()
-    min_phone_contact = Contact.query.filter(Contact.phoneNumber == phoneNumber).order_by(Contact.id).first()
+        if min_phone_contact.id != min_email_contact.id:
+            min_email_contact_id = min_email_contact.id
+            min_phone_contact_id = min_phone_contact.id
+            if min_email_contact_id < min_phone_contact_id:
+                smaller_id = min_email_contact_id
+                larger_contact = min_phone_contact
+            else:
+                smaller_id = min_phone_contact_id
+                larger_contact = min_email_contact
 
-    if min_phone_contact.id != min_email_contact.id:
-        min_email_contact_id = min_email_contact.id
-        min_phone_contact_id = min_phone_contact.id
-        if min_email_contact_id < min_phone_contact_id:
-            smaller_id = min_email_contact_id
-            larger_contact = min_phone_contact
-        else:
-            smaller_id = min_phone_contact_id
-            larger_contact = min_email_contact
-
-        # change the larger ID's linkPrecedence to secondary
-        larger_contact.linkPrecedence = os.getenv('SECONDARY')
-        # add smaller ID in its linkedId
-        larger_contact.linkedId = smaller_id
-        db.session.commit()
+            # change the larger ID's linkPrecedence to secondary
+            larger_contact.linkPrecedence = os.getenv('SECONDARY')
+            # add smaller ID in its linkedId
+            larger_contact.linkedId = smaller_id
+            db.session.commit()
 
     # Query the Contact entity for objects with the same email or phoneNumber
-    contacts = Contact.query.filter(or_(Contact.email == email, Contact.phoneNumber == phoneNumber)).all()
+    contacts = Contact.query.filter(or_(Contact.email == identity_request.email,
+                                        Contact.phoneNumber == identity_request.phoneNumber)).all()
 
     return contacts
 
