5956e823922b4b5034bc4b38d25688ff849a7286
yash-pathak-1997 <yashhpathakk1997@gmail.com>
2024-05-24 15:56:16 +0530

    Removed all hard-coded string literals from repo.

--- entity/Contact.py
+++ entity/Contact.py
@@ -1,11 +1,12 @@
+import os
 from datetime import datetime
 from api import db
 
 
 # Define the Contact entity
 class Contact(db.Model):
-    __tablename__ = 'contacts'
-    __table_args__ = {'schema': 'identity'}
+    __tablename__ = os.getenv('CONTACT_TABLE')
+    __table_args__ = {'schema': os.getenv('SCHEMA')}
 
     id = db.Column(db.Integer, primary_key=True)
     phoneNumber = db.Column(db.String)
--- service/identity_service.py
+++ service/identity_service.py
@@ -1,6 +1,5 @@
 import json
 import os
-
 from dao import identity_dao
 from model import IdentityRequestModel, IdentityResponseModel
 
